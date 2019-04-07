84db4e5c642ad2dd6d9768d8dc2df76df2132874
frank <489280591@qq.com>
2019-04-07 22:34:17 +0800

    no message

---
+++ Assets/ScriptNodeFlow/core/Binding.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ScriptNodeFlow
+{
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BindingFlow:Attribute
+    {
+        public BindingFlow(string id)
+        {
+            ID = id;
+        }
+        public string ID;
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BindingNode:Attribute
+    {
+        public BindingNode(string id)
+        {
+            ID = id;
+        }
+        public string ID;
+    }
+
+    [AttributeUsage(AttributeTargets.Class)]
+    public class BindingRouter:Attribute
+    {
+        public BindingRouter(string routerId,string coditionId)
+        {
+            RouterID = routerId;
+            CoditionId = coditionId;
+        }
+        public string RouterID;
+        public string CoditionId;
+    }
+}
